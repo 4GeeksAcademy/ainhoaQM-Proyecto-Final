7373d5070cc64d0f26e85d3486afadbe53ad65d2
Ainhoa <113107823+AinhoaQM@users.noreply.github.com>
2023-10-26 23:23:42 +0000

    arreglando botones de cards + y - para que se modifique individualmente, impidiendo que se puedan hacer orders de valor 0 y añadiendo ruta de horario

--- src/front/js/pages/payment.js
+++ src/front/js/pages/payment.js
@@ -1,28 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
 
-//stripe
-import { loadStripe } from '@stripe/stripe-js';
-import { Elements } from '@stripe/react-stripe-js';
-import { FormPayment } from "../component/formPayment";
-import "../../styles/stripe.css";
-
 export const Payment = () => {
   const [totalPrice, setTotalPrice] = useState(null);
   let { orderId } = useParams();
-  const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
-  const [clientSecret, setClientSecret] = useState("");
-  const appearance = {
-    theme: 'stripe',
-    variables: {
-      colorPrimary: '#ff7e67',
-      colorText: '#474852',
-    },
-  };
-  const options = {
-    clientSecret,
-    appearance,
-  };
 
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -48,30 +29,12 @@ export const Payment = () => {
     });
   }, [orderId]);
   
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    fetch(process.env.BACKEND_URL + "/api/process-payment", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${token}`,
-      },
-      body: JSON.stringify({ totalPrice }),
-    })
-      .then((res) => res.json())
-      .then((data) => setClientSecret(data.clientSecret));
-  }, []);
 
   return (
     <div className="Payment">
       <p className="fs-1">{orderId}</p>
       {totalPrice !== null && (
         <p>Total Price: {totalPrice}</p>
-      )}
-      {clientSecret && (
-        <Elements stripe={stripePromise} options={options} >
-          <FormPayment />
-        </Elements>
       )}
     </div>
   );
